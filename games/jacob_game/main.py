8db3babb3bc2e6e81606eb573452078f6d8606d8
Jacob Rice <psjacob715@gmail.com>
2023-10-19 23:14:15 -0500

    Worked on game -Jacob

--- games/jacob_game/main.py
+++ games/jacob_game/main.py
@@ -1,43 +1,107 @@
-import pygame, pygame.locals
+import pygame
 from settings import *
 
+# TODO: Make platform class, update graphics, literally finish the game by wednesday, etc.
+
+# The player character 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
         
-        self.surf = pygame.image.load("games/jacob_game/images/player.png").convert()
+        # PLAYER SETTINGS
+        # TODO: change
+        self.side_length = 100   # l and w of rect (square)
+        self.dimensions = (self.side_length, self.side_length)
+        self.x = WIDTH // 2
+        self.y = 500
+        self.movement_speed = 5
+        self.gravity = 0
         
-        # TODO: CHANGE THIS TO MAKE IT WORK
-        self.surf.set_colorkey((255, 255, 255), pygame.locals.RLEACCEL)
+        self.surf = pygame.image.load("games/jacob_game/images/player.png")
+        self.surf = pygame.transform.scale(self.surf, self.dimensions)
         
-        # Resize sprite to be bigger on screen
-        self.surf = pygame.transform.scale(self.surf, (200, 200))
+        self.player_rect = self.surf.get_rect(midbottom=(WIDTH//2, 600))
         
-        # Print sprite size (debug)
+        '''print sprite size (debug)'''
         #print(self.surf.get_size())
         
-        self.rect = self.surf.get_rect()
-        
     # Handle user input
-    def update(self):
-        pass
+    """
+        For now, this function takes keyboard input.
+        This will be changed when controller is implemented.
+        Do I have any idea how that will work? notyet
+    """
+    def update(self, pressed):
+        if pressed[K_LEFT]:
+            self.player_rect.x -= self.movement_speed
+        
+        elif pressed[K_RIGHT]:
+            self.player_rect.x += self.movement_speed
+        
+        #elif pressed[K_SPACE]:
+            #self.gravity = -20  # physics?
+            #space_down = True
 
+# Platforms you gotta jump on
+class Platform(pygame.sprite.Sprite):
+    def __init__(self,):
+        super(Platform, self).__init__()
+        pass
+            
 def main():
     
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     screen.fill((255, 255, 255))
+    
     player = Player()
+    
+    # sky
+    bg = pygame.image.load("games/jacob_game/images/sky.png")
+    bg = pygame.transform.scale(bg, (WIDTH, HEIGHT))
+    
+    # ground that the game starts on
+    ground = pygame.image.load("games/jacob_game/images/ground.png")
+    ground = pygame.transform.scale(ground, (WIDTH, 800))   # wide as screen and long af
+    
+    # TODO: clean code up
+    ground_rect = ground.get_rect()
+    
+    # handle fps
+    clock = pygame.time.Clock()
     
     # Main game loop
     RUNNING = True
     while RUNNING:
         for event in pygame.event.get():
-            if event.type == pygame.locals.QUIT:
+            if event.type == QUIT:
                 RUNNING = False
         
-        screen.blit(player.surf, player.rect)
+        # handle user input
+        pressed_keys = pygame.key.get_pressed()
+        player.update(pressed_keys)
+        
+        #player is constantly jumping
+        if player.player_rect.midbottom[1] == 600:
+            player.gravity = -20
+            
+        # gravity does be existing tho
+        player.gravity += 1
+        player.player_rect.y += player.gravity
+        
+        # keep player on ground
+        if player.player_rect.midbottom[1] > 600:
+            player.player_rect.midbottom[1] == 600
+        
+        # update screen
+        screen.blit(bg, (0, 0))
+        screen.blit(ground, (0, 600))
+        screen.blit(player.surf, player.player_rect)
+        
         pygame.display.flip()
+        
+        # keeps game at 60FPS
+        clock.tick(FPS)
     
     # If game loop is broken
     exit()
--- games/jacob_game/settings.py
+++ games/jacob_game/settings.py
@@ -1,3 +1,10 @@
+from pygame.locals import (
+    K_RIGHT,
+    K_LEFT,
+    K_SPACE,
+    QUIT,
+)
+
 # Game window
 WIDTH = 1000
 HEIGHT = 800
