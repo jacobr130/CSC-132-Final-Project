80c9d71e6d6450728b6159b0c6be90432fba1d8e
Jacob Rice <psjacob715@gmail.com>
2023-10-23 19:09:25 -0500

    Update game settings -Jacob

--- games/jacob_game/settings.py
+++ games/jacob_game/settings.py
@@ -6,8 +6,16 @@ from pygame.locals import (
 )
 
 # Game window
-WIDTH = 1000
-HEIGHT = 800
+from screeninfo import get_monitors
+
+# this makes the game fullscreen no matter what
+# monitor is being used
+for monitor in get_monitors():
+    if monitor.is_primary == True:
+        screen = monitor
+
+WIDTH = screen.width
+HEIGHT = screen.height
 
 # In-game
 FPS = 60
