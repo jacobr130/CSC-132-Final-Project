1e3ba278f5a487059c3ad56ab2ee6cc58b91c9b1
Jacob Rice <psjacob715@gmail.com>
2023-10-24 23:55:10 -0500

    Update jacob_game settings

--- games/jacob_game/settings.py
+++ games/jacob_game/settings.py
@@ -2,6 +2,8 @@ from pygame.locals import (
     K_RIGHT,
     K_LEFT,
     K_SPACE,
+    K_RETURN,
+    K_ESCAPE,
     QUIT,
 )
 
@@ -23,11 +25,16 @@ texture_player_l = "games\jacob_game\gfx\player_l.png"
 texture_floor = "games\jacob_game\gfx\ground.png"
 texture_bg = "games\jacob_game\gfx\sky.png"
 texture_platform_short = "games\jacob_game\gfx\platform_short.png"
+font_retro = "games\jacob_game\gfx\computer.ttf"
 
 # In-game
 FPS = 60
 WORLD_GRAVITY = 0.8
 
 # Player settings
-PLAYER_ACCELERATION = 3
+PLAYER_ACCELERATION = 2
 PLAYER_FRICTION = -0.12
+
+# colors
+BG_COLOR = (34, 44, 48)
+GREEN = (0, 255, 0)
