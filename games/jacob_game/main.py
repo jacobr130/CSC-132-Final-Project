faa9b45bb73b300762b221c246d9a7405761c5dc
Jacob Rice <psjacob715@gmail.com>
2023-10-24 20:05:26 -0500

    Overhaul game physics

--- games/jacob_game/main.py
+++ games/jacob_game/main.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.math import Vector2
 from random import randint
 from settings import *
 
@@ -10,43 +11,57 @@ class Player(pygame.sprite.Sprite):
         # PLAYER SETTINGS
         self.side_length = 150   # l and w of rect (square)
         self.dimensions = (self.side_length, self.side_length)
-        self.movement_speed = 10
         self.gravity = 0
         self.jump_height = -25  # gravity offset
         
         self.surf = pygame.image.load(texture_player_r).convert_alpha()
         self.surf = pygame.transform.scale(self.surf, self.dimensions)
         
-        self.rect = self.surf.get_rect(midbottom=(WIDTH//2, 800),
-                                              bottomright=((WIDTH//2) + self.side_length, 800),
-                                              bottomleft=((WIDTH//2) - self.side_length, 800))
+        self.rect = self.surf.get_rect(midbottom=(WIDTH//2, 800))
         self.hitbox = self.rect.inflate(-35, 0)
         
+        self.pos = Vector2(self.rect.topleft[0], self.rect.topleft[1])
+        self.velocity = Vector2(0, 0)
+        self.acceleration = Vector2(0, 0)
+        
     # Handle user input
-    """
-        For now, this function takes keyboard input.
-        This will be changed when controller is implemented.
-        Do I have any idea how that will work?
-    """
     def update(self, pressed):
+        """
+            For now, this function takes keyboard input.
+            This will be changed when controller is implemented.
+            Do I have any idea how that will work?
+        """
+        self.acceleration = Vector2(0, WORLD_GRAVITY)
+        
         if pressed[K_LEFT]:
             self.surf = pygame.image.load(texture_player_l).convert_alpha()
             self.surf = pygame.transform.scale(self.surf, self.dimensions)
             
-            self.rect.x -= self.movement_speed
-            # if player goes off left of screen spawn at right
-            if self.rect.bottomleft[0] == 0 - self.side_length:     
-                self.rect.x = WIDTH + self.side_length
+            self.acceleration.x = PLAYER_ACCELERATION * -1
         
-        elif pressed[K_RIGHT]:
+        if pressed[K_RIGHT]:
             self.surf = pygame.image.load(texture_player_r).convert_alpha()
             self.surf = pygame.transform.scale(self.surf, self.dimensions)
             
-            self.rect.x += self.movement_speed
-            # if player goes off right of screen spawn at left
-            if self.rect.bottomright[0] == WIDTH + self.side_length:
-                self.rect.x = 0 - self.side_length
-                
+            self.acceleration.x = PLAYER_ACCELERATION
+        
+        # physics
+        self.acceleration.x += self.velocity.x * PLAYER_FRICTION
+        self.velocity += self.acceleration
+        self.pos += self.velocity + 0.5 * self.acceleration
+        self.rect.x = self.pos.x
+        self.rect.y = self.pos.y
+        
+        # if player goes off left of screen spawn at right
+        if self.rect.right <= 0 - self.side_length:
+            print("left side triggering")     
+            self.pos.x = WIDTH + self.side_length   # updating rect.x by pos so pos is changing
+        
+        # if player goes off right of screen spawn at left   
+        if self.rect.left >= WIDTH + self.side_length:
+            self.pos.x = 0 - self.side_length 
+        
+            
         """
         # elif pressed[K_SPACE]:
             # self.gravity = -20  # physics?
@@ -88,7 +103,8 @@ class Platform(pygame.sprite.Sprite):
             
 def main():
 
-    collidables = []
+    #collidables = []
+    collidables2 = pygame.sprite.Group()   # maybe later
     
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -100,14 +116,16 @@ def main():
     bg = pygame.transform.scale(bg, (WIDTH, HEIGHT))
     
     # ground that the game starts on
-    ground = pygame.image.load(texture_floor).convert()
-    ground = pygame.transform.scale(ground, (WIDTH + (player.side_length*2), 800))
-    ground_rect = ground.get_rect(topleft=(0, 800))
-    collidables.append(ground_rect)
+    ground = pygame.sprite.Sprite()
+    ground.surf = pygame.image.load(texture_floor).convert()
+    ground.surf = pygame.transform.scale(ground.surf, (WIDTH*2, 800))
+    ground.rect = ground.surf.get_rect(topleft=(-800, 800))
+    #collidables.append(ground_rect)
+    collidables2.add(ground)
     
     # platforms
     platforms = []
-    platform = Platform()
+    #platform = Platform()
     #collidables.append(platform.rect)
     
     # handle fps
@@ -126,35 +144,31 @@ def main():
         
         # this may cause a bug later
         for platform in platforms:
-            collidables.append(platform.rect)
-        
-        #for i in platforms:
-            #print(f"num: {len(platforms)}")    # debug
-            #print(i.rect)
-
+            collidables2.add(platform)
+            
         # handle user input
         pressed_keys = pygame.key.get_pressed()
         player.update(pressed_keys)
         
         # player is constantly jumping; check for collisions between 
-        # player and all other objects
-        ## TODO: fix platform jumping bug: jump through platforms without
-        ## auto jumping
+        # player and all other objects if player is falling
         player.hitbox.center = player.rect.center
-        for rect in collidables:
-            if player.hitbox.colliderect(rect):
-                player.gravity = player.jump_height
-            
-        player.gravity += WORLD_GRAVITY
-        player.rect.y += player.gravity
+        if player.velocity.y > 0:
+            collision = pygame.sprite.spritecollide(player, collidables2, False)
+            if collision and player.hitbox.colliderect(collision[0].rect):
+                if player.hitbox.bottom < collision[0].rect.bottom:
+                    player.pos.y = collision[0].rect.top - player.side_length    # because pos is rect topleft
+                    player.rect.y = player.pos.y
+
+                    player.velocity.y = player.jump_height
         
         # update screen
         screen.blit(bg, (0, 0))
-        screen.blit(ground, ground_rect)
+        screen.blit(ground.surf, ground.rect)
         screen.blit(player.surf, player.rect)
         for platform in platforms:
             screen.blit(platform.surf, platform.rect)
-        #screen.blit(platform.surf, (100, 700))     # debug
+        #screen.blit(platform.surf, platform.rect)     # debug
         
         pygame.display.flip()
         
